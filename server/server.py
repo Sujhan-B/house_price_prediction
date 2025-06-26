3ce95c373b00d2cc4c7a4dc568f24f4ee56d9bc7
Sujhan B <sujhanbharathan@hotmail.com>
2025-06-26 17:32:05 +0530

    first-commit

---
+++ server/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route("/get-location-names")
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route("/predict-home-price", methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bath = int(request.form['bath'])
+    bhk = int(request.form['bhk'])
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bath, bhk)
+    }) 
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == '__main__':
+    print("starting flask server for House Price prediction")
+    util.load_saved_artifacts()  # Ensure artifacts are loaded before serving requests
+    app.run()
